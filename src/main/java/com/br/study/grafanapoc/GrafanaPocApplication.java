2586df7c92120d95cd5a30108391f49a94f35b9d
Adri Silva <adrisilva0002@gmail.com>
2021-07-29 20:10:52 -0300

    First Commit

---
+++ src/main/java/com/br/study/grafanapoc/GrafanaPocApplication.java
@@ -0,0 +1,13 @@
+package com.br.study.grafanapoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrafanaPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrafanaPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/study/grafanapoc/GrafanaPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.study.grafanapoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrafanaPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
